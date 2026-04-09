e3c4f8149508aac3b049a6cb09a5facf12f04342
muhammadyusuforifjonov20070330-eng <muhammadyusuf.orifjonov20070330@gmail.com>
2026-04-09 21:49:29 +0500

    Add CORS middleware and server startup
    Added CORS middleware to handle cross-origin requests and updated server startup code.

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Musiqa eshitaylik", docs_url='/')
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.post("/upload")
 def upload(file: UploadFile = File(...)):
     return save_file(file)
@@ -17,10 +25,8 @@ def fayl_korish(filename: str):
         return {"error": "Fayl topilmadi"}
     return FileResponse(path, media_type="audio/mpeg")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
+# 🔥 SERVER QO‘SHILDI
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
